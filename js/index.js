29d0d77e298cbcae4fe461378cc3f1232e4ffe70
Lherajt <64788512+Lherajt@users.noreply.github.com>
2021-02-18 08:59:10 -0500

    Resume & added background

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const navList = document.querySelector('.nav__list')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
